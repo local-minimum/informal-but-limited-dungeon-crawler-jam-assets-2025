7035f3a107bbdf17f2334f9a4f6e2f9d63e842da
Jimb Esser <wasteland@gmail.com>
2025-10-22 12:32:39 -0700

    Add script to push to itch.io via Butler

---
+++ distro/butler_push.cmd
@@ -0,0 +1,5 @@
+butler push out/tiles/atlas.png captaincoder/limited-dc-jam-2025-assets:atlas.png
+butler push out captaincoder/limited-dc-jam-2025-assets:everything
+butler push out captaincoder/limited-dc-jam-2025-assets:graphics --ignore audio
+butler push out captaincoder/limited-dc-jam-2025-assets:audio-mp3 --ignore tiles --ignore fonts --ignore README.md --ignore ogg
+butler push out captaincoder/limited-dc-jam-2025-assets:audio-ogg --ignore tiles --ignore fonts --ignore README.md --ignore mp3
